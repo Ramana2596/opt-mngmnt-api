21741c47a6ff09c4f982801c2b6eccc56881b7a8
Ramana <muthuramana96@gmail.com>
2024-12-24 18:55:38 +0530

    added operation plan info input api

--- opt-mngmnt/getOperationalPlanInfoInput.js
+++ opt-mngmnt/getOperationalPlanInfoInput.js
@@ -5,20 +5,23 @@ const moment = require('moment');
 const router = express.Router();
 
 sql.connect(dbConfig).then(() => {
-    router.get('/getMarketFactorInfoInput', async (req, res) => {
+    router.get('/getOperationalPlanInfoInput', async (req, res) => {
             try {
                 //Convert recieved date into SQL readable format
                 const date = req?.query?.productionMonth !== 'null' ? `'${moment(req?.query?.productionMonth, 'YYYY-MM-DD').format('YYYY-MM-DD')}'` : null;
                 //Frame SQL query to get executed
-                const query = `EXEC [dbo].[UI_Market_Factor_Query]
+                const query = `EXEC [dbo].[UI_Ops_Business_Plan_Query]
                                     @Game_id = ${req?.query?.gameId !== 'null' ? `'${req?.query?.gameId}'` : null},
 		                            @Game_Batch = ${req?.query?.gameBatch !== 'null' ? req?.query?.gameBatch : null},
+                                    @Game_Team = ${req?.query?.gameTeam !== 'null' ? req?.query?.gameTeam : null},
 		                            @Production_Month = ${date},
-		                            @Market_Input_Id = ${req?.query?.marketInputId !== 'null' ? `'${req?.query?.marketInputId}'` : null},
+                                    @Operations_Input_Id = ${req?.query?.operationsInputId !== 'null' ? `'${req?.query?.operationsInputId}'` : null},
 		                            @Part_Category = ${req?.query?.partCategory !== 'null' ? `'${req?.query?.partCategory}'` : null},
 		                            @Ref_Type_Info = ${req?.query?.refTypeInfo !== 'null' ? `'${req?.query?.refTypeInfo}'` : null},
 		                            @Ref_Type_Price = ${req?.query?.refTypePrice !== 'null' ? `'${req?.query?.refTypePrice}'` : null},
+                                    @Market_Input_Id = ${req?.query?.marketInputId !== 'null' ? `'${req?.query?.marketInputId}'` : null},
 		                            @CMD_Line = ${req?.query?.cmdLine !== 'null' ? `'${req?.query?.cmdLine}'` : null}`;
+                console.log(query);
                 const result = await sql.query(query);
                 res.json(result.recordset);
             } catch (err) {
