00db1032fca899a7bbe6465efe718cf1c67f59ab
Ramana <muthuramana96@gmail.com>
2025-03-09 14:45:12 +0530

    operational decision input apo update

--- opt-mngmnt/getOperationalPlanInfoInput.js
+++ opt-mngmnt/getOperationalPlanInfoInput.js
@@ -6,11 +6,11 @@ const router = express.Router();
 
 sql.connect(dbConfig).then(() => {
     router.get('/getOperationalPlanInfoInput', async (req, res) => {
-            try {
-                //Convert recieved date into SQL readable format
-                const date = req?.query?.productionMonth ? `'${moment(req?.query?.productionMonth, 'YYYY-MM-DD').format('YYYY-MM-DD')}'` : null;
-                //Frame SQL query to get executed
-                const query = `EXEC [dbo].[UI_Ops_Business_Plan_Query]
+        try {
+            //Convert recieved date into SQL readable format
+            const date = req?.query?.productionMonth ? `'${moment(req?.query?.productionMonth, 'YYYY-MM-DD').format('YYYY-MM-DD')}'` : null;
+            //Frame SQL query to get executed
+            const query = `EXEC [dbo].[UI_Ops_Business_Plan_Query]
                                     @Game_id = ${req?.query?.gameId ? `'${req?.query?.gameId}'` : null},
 		                            @Game_Batch = ${req?.query?.gameBatch ? req?.query?.gameBatch : null},
                                     @Game_Team = ${req?.query?.gameTeam ? req?.query?.gameTeam : null},
@@ -20,14 +20,17 @@ sql.connect(dbConfig).then(() => {
 		                            @Ref_Type_Info = ${req?.query?.refTypeInfo ? `'${req?.query?.refTypeInfo}'` : null},
 		                            @Ref_Type_Price = ${req?.query?.refTypePrice ? `'${req?.query?.refTypePrice}'` : null},
                                     @Market_Input_Id = ${req?.query?.marketInputId ? `'${req?.query?.marketInputId}'` : null},
+                                    @Quantity_Id = ${req?.query?.quantityId ? `'${req?.query?.quantityId}'` : null},
+                                    @Price_Id = ${req?.query?.priceId ? `'${req?.query?.priceId}'` : null},
+                                    @Part_no = ${req?.query?.partNo ? `'${req?.query?.partNo}'` : null},
 		                            @CMD_Line = ${req?.query?.cmdLine ? `'${req?.query?.cmdLine}'` : null}`;
-                console.log(query);
-                const result = await sql.query(query);
-                res.json(result.recordset);
-            } catch (err) {
-                console.error('Query failed:', err);
-                res.status(500).send('Internal Server Error');
-            }
+            console.log(query);
+            const result = await sql.query(query);
+            res.json(result.recordset);
+        } catch (err) {
+            console.error('Query failed:', err);
+            res.status(500).send('Internal Server Error');
+        }
     });
 });
 
