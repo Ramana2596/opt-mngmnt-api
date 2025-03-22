e26f293f23d51c8e346b518fa524ceb925c65810
SivaGita <sivagi.mu@gmail.com>
2025-03-22 12:45:58 +0530

    Three new files added

---
+++ opt-mngmnt/getRBACInfo.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const sql = require('mssql');
+const dbConfig = require('../dbConfig');
+const router = express.Router();
+
+sql.connect(dbConfig).then(() => {
+    router.get('/getRBACInfo', async (req, res) => {
+        try {
+            const result = await sql.query(`
+   EXEC [dbo].[UI_RBAC_Screen_Info]
+        @Game_Id = '${req.query.gameId}'`);
+            res.json(result.recordset);
+        } catch (err) {
+            console.error('Query failed:', err);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+});
+
+module.exports = router;
---
+++ opt-mngmnt/getReferenceInfo.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const sql = require('mssql');
+const dbConfig = require('../dbConfig');
+const router = express.Router();
+
+sql.connect(dbConfig).then(() => {
+    router.get('/getReferenceInfo', async (req, res) => {
+        try {
+            const result = await sql.query(`
+   EXEC [dbo].[UI_Reference_Mst_Info]
+        @Game_Id = '${req.query.gameId}'`);
+            res.json(result.recordset);
+        } catch (err) {
+            console.error('Query failed:', err);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+});
+
+module.exports = router;
