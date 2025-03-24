c752641e1e2f4dff57524514b16e2e34a985b624
SivaGita <sivagi.mu@gmail.com>
2025-03-24 14:51:38 +0530

    Three js files -Mktfactor,Strategy Launch & Plan added

---
+++ opt-mngmnt/getMarketFactorAllInfo.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const sql = require('mssql');
+const dbConfig = require('../../dbConfig');
+const router = express.Router();
+
+sql.connect(dbConfig).then(() => {
+    router.get('/getMarketFactorAllInfo', async (req, res) => {
+        try {
+            const result = await sql.query(`
+   EXEC [dbo].[UI_Market_Factor_All_Info] 
+        @Game_Id = ${req?.query?.gameId ? `${req.query.gameId}` : 'NULL'},
+        @Game_Batch = ${req?.query?.gameBatch ? req.query.gameBatch : 'NULL'},
+        @CMD_Line = '${req.query.cmdLine}'`);
+            res.json(result.recordset);
+        } catch (err) {
+            console.error('Query failed:', err);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+});
+
+module.exports = router;
---
+++ opt-mngmnt/getStrategyLaunchInfo.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const sql = require('mssql');
+const dbConfig = require('../../dbConfig');
+const router = express.Router();
+
+sql.connect(dbConfig).then(() => {
+    router.get('/getStrategyLaunchInfo', async (req, res) => {
+        try {
+            const result = await sql.query(`
+   EXEC [dbo].[UI_Strategy_Launch_Info] 
+        @Game_Id = ${req?.query?.gameId ? `${req.query.gameId}` : 'NULL'}`);
+            res.json(result.recordset);
+        } catch (err) {
+            console.error('Query failed:', err);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+});
+
+module.exports = router;
