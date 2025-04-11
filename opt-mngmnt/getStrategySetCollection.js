3ec88eaccf6194de279f8377983619e036ae9aa9
SivaGita <sivagi.mu@gmail.com>
2025-04-11 20:03:46 +0530

    "Added new API Call.  QA:  SP tested OK in SQL Server"

---
+++ opt-mngmnt/getStrategySetCollection.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const sql = require('mssql');
+const dbConfig = require('../dbConfig');
+const router = express.Router();
+
+sql.connect(dbConfig).then(() => {
+    router.get('/getStrategySetCollection', async (req, res) => {
+        try {
+            const result = await sql.query(`
+   EXEC [dbo].[UI_Strategy_Set_Collection]
+        @Game_Id = '${req.query.gameId}'
+//        @Game_Batch = ${req.query.gameBatch},
+//        @Game_Team = '${req.query.gameTeam}'
+        `);
+            res.json(result.recordset);
+        } catch (err) {
+            console.error('Query failed:', err);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+});
+
+module.exports = router;
