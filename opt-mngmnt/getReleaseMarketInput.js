945306ee792cfceefbaff8be023a4dfbb9d26249
SivaGita <sivagi.mu@gmail.com>
2025-05-01 20:39:25 +0530

    "New API Call for upload  of  Std Market Input , - an alternative to keying in  data.
    Action Button  is to be included under Team Play"

---
+++ opt-mngmnt/getReleaseMarketInput.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const sql = require('mssql');
+const dbConfig = require('../dbConfig');
+const router = express.Router();
+
+sql.connect(dbConfig).then(() => {
+    router.get('/getReleaseMarketInput', async (req, res) => {
+        try {
+            const result = await sql.query(`
+   EXEC [dbo].[UI_Release_Market_Input]
+        @Game_Id = '${req.query.gameId}',
+        @Game_Batch = ${req.query.gameBatch},
+        @Game_Team = '${req.query.gameTeam}'`);
+            res.json(result.recordset);
+        } catch (err) {
+            console.error('Query failed:', err);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+});
+
+module.exports = router;
