52daa43b412de0137d6952a2c37f7a7994a2f98d
SivaGita <sivagi.mu@gmail.com>
2025-05-02 14:41:46 +0530

    "New API for UI Page ProfessionRoleInfo UI 25 070 Testing: OK SQL Query :  Parameter @Game_Id Okay ; fetches rows "

---
+++ opt-mngmnt/getProfessionRoleInfo.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const sql = require('mssql');
+const dbConfig = require('../dbConfig');
+const router = express.Router();
+
+sql.connect(dbConfig).then(() => {
+    router.get('/getProfessionRoleInfo', async (req, res) => {
+        try {
+            const result = await sql.query(`
+   EXEC [dbo].[UI_Profession_Info]
+        @Game_Id = '${req.query.gameId}'
+         `);
+            res.json(result.recordset);
+        } catch (err) {
+            console.error('Query failed:', err);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+});
+
+module.exports = router;
