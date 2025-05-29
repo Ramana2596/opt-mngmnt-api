ca257fee70ad7532bb1441317a914ffd6f17c252
SivaGita <sivagi.mu@gmail.com>
2025-05-29 09:35:39 +0530

    Renamed File name and API SP

--- opt-mngmnt/getReleaseStdMarketInput.js
+++ opt-mngmnt/getReleaseStdMarketInput.js
@@ -4,10 +4,10 @@ const dbConfig = require('../dbConfig');
 const router = express.Router();
 
 sql.connect(dbConfig).then(() => {
-    router.get('/getStdMarketInput', async (req, res) => {
+    router.get('/getReleaseStdMarketInput', async (req, res) => {
         try {
             const result = await sql.query(`
-   EXEC [dbo].[UI_Std_Market_Input]
+   EXEC [dbo].[UI_Release_Std_Market_Input]
         @Game_Id = '${req.query.gameId}',
         @Game_Batch = ${req.query.gameBatch},
         @Game_Team = '${req.query.gameTeam}'`);
