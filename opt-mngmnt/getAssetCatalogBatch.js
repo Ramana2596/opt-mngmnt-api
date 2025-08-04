c2c803dbae47854dff8fc7a316d47f5cc326b1fa
SivaGita <sivagi.mu@gmail.com>
2025-08-04 18:39:27 +0530

    API Call Asset CAtalog (Master) of Batch added exclsuively

---
+++ opt-mngmnt/getAssetCatalogBatch.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const sql = require('mssql');
+const dbConfig = require('../dbConfig');
+const router = express.Router();
+
+sql.connect(dbConfig).then(() => {
+    router.get('/getAssetCatalogBatch', async (req, res) => {
+        try {
+            const result = await sql.query(`
+                EXEC [dbo].[UI_Asset_Catalog_Batch]
+                        @Game_Id = '${req.query.gameId}',
+                        @Game_Batch = ${req.query.gameBatch}`);
+                res.json(result.recordset);
+        } catch (err) {
+            console.error('Query failed:', err);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+});
+
+module.exports = router;
