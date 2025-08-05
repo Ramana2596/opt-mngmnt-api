0b1e1f361e9a07c93b017c06ecc463fdd63c28e1
SivaGita <sivagi.mu@gmail.com>
2025-08-05 16:35:06 +0530

    Comment lines are added

--- opt-mngmnt/utils/paramTypes.js
+++ opt-mngmnt/utils/paramTypes.js
@@ -1,6 +1,7 @@
+
 // centralized parameter dictionary
 const sql = require('mssql');
-
+// This file defines List of Parameter (Key) Versus Data type ,  used in various SQL queries
 const paramTypes = {
   Game_Id: sql.NVarChar(20),
   Game_Batch: sql.Int,
