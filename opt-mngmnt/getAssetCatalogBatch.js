31fa2215bd1e360f127a5e0b72a1e0718ddbd28e
SivaGita <sivagi.mu@gmail.com>
2025-08-05 15:47:02 +0530

    Added parameter @CMD_Line

--- opt-mngmnt/getAssetCatalogBatch.js
+++ opt-mngmnt/getAssetCatalogBatch.js
@@ -7,9 +7,10 @@ sql.connect(dbConfig).then(() => {
     router.get('/getAssetCatalogBatch', async (req, res) => {
         try {
             const result = await sql.query(`
-                EXEC [dbo].[UI_Asset_Catalog_Batch]
-                        @Game_Id = '${req.query.gameId}',
-                        @Game_Batch = ${req.query.gameBatch}`);
+                    EXEC [dbo].[UI_Asset_Catalog_Batch]
+                            @Game_Id = '${req.query.gameId}',
+                            @Game_Batch = ${req.query.gameBatch},
+                            @CMD_Line = '${req.query.cmdLine}'`);
                 res.json(result.recordset);
         } catch (err) {
             console.error('Query failed:', err);
