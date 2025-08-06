e88d3e0feb5499fca742e46b38b0cf4d12db24d0
SivaGita <sivagi.mu@gmail.com>
2025-08-06 15:12:01 +0530

    Deleted the commented previous SQL Query  and Commented log

--- opt-mngmnt/getMfgRoutingInfo.js
+++ opt-mngmnt/getMfgRoutingInfo.js
@@ -2,35 +2,19 @@ const express = require('express');
 const sql = require('mssql');
 const dbConfig = require('../dbConfig');
 const router = express.Router();
-/*
-sql.connect(dbConfig).then(() => {
-    router.get('/getMfgRoutingInfo', async (req, res) => {
-        try {
-            const result = await sql.query(`
-   EXEC [dbo].[UI_Mfg_Routing_Info]
-        @Game_Id = '${req.query.gameId}',
-        @Game_Batch = ${req.query.gameBatch},
-        @Game_Team = '${req.query.gameTeam}'`);
-            res.json(result.recordset);
-        } catch (err) {
-            console.error('Query failed:', err);
-            res.status(500).send('Internal Server Error');
-        }
-    });
-});
-*/
+
 
 sql.connect(dbConfig).then(() => {
     router.get('/getMfgRoutingInfo', async (req, res) => {
         try {
-            
+           /* 
             // Log the incoming parameters
             console.log('Request for getProductMstInfo:', {
                 Game_Id: req.query.gameId,
                 Game_Batch: req.query.gameBatch,
                 Game_Team: req.query.gameTeam
             });
-            
+           */ 
             const request = new sql.Request();
 
             // Use correct types based on SQL Server stored procedure
