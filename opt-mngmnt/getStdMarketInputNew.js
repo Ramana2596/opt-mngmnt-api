dce9cd508ec421275e6e521eaf0d324c386f76c4
SivaGita <sivagi.mu@gmail.com>
2025-08-07 16:07:38 +0530

    API Call getStdMarketInputNew Existing Block of Codes on 'sql connect ...' commented It is replaced with new code for secure data types as in SP

--- opt-mngmnt/getStdMarketInputNew.js
+++ opt-mngmnt/getStdMarketInputNew.js
@@ -36,6 +36,7 @@ const sql = require('mssql');
 const dbConfig = require('../dbConfig');
 const router = express.Router();
 
+/*
 sql.connect(dbConfig).then(() => {
     router.get('/getStdMarketInputNew', async (req, res) => {
         try {
@@ -50,5 +51,28 @@ sql.connect(dbConfig).then(() => {
         }
     });
 });
+*/
+
+sql.connect(dbConfig).then(() => {
+    router.get('/getStdMarketInputNew', async (req, res) => {
+        try {
+            
+            const request = new sql.Request();
+
+            // Use correct data types based on SQL Server stored procedure
+            request.input('Game_Id', sql.NVarChar, req.query.gameId);
+            request.input('Game_Batch', sql.Int, parseInt(req.query.gameBatch));
+            request.input('Game_Team', sql.NVarChar, req.query.gameTeam);
+
+            const result = await request.execute('UI_Std_Market_Input_New');
+
+            res.json(result.recordset);
+        } catch (err) {
+            console.error('Query failed:', err);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+});
+
 
 module.exports = router;
