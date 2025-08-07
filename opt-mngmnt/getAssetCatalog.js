46f7ac63fde0cdd9cabec362eb5e169e50531273
SivaGita <sivagi.mu@gmail.com>
2025-08-07 13:11:57 +0530

    Moved  'sql.connect --' inside handler to await DB connectivty.

--- opt-mngmnt/getAssetCatalog.js
+++ opt-mngmnt/getAssetCatalog.js
@@ -3,12 +3,15 @@ const sql = require('mssql');
 const dbConfig = require('../dbConfig');
 const router = express.Router();
 
-sql.connect(dbConfig).then(() => {
+//sql.connect(dbConfig).then(() => {
     router.get('/AssetCatalog', async (req, res) => {
         try {
-            
+            await sql.connect(dbConfig);
+
             const request = new sql.Request();
 
+            console.log('Type of sql.Request:', typeof sql.Request); // Should log: 'function'
+           
             // Use correct types based on SQL Server stored procedure
             request.input('Game_Id', sql.NVarChar, req.query.gameId);
             request.input('Game_Batch', sql.Int, parseInt(req.query.gameBatch));
@@ -22,6 +25,6 @@ sql.connect(dbConfig).then(() => {
             res.status(500).send('Internal Server Error');
         }
     });
-});
+//});
 
 module.exports = router;
