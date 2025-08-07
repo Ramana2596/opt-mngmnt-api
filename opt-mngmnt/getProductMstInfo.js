d08a40e1471002b9fa53d82a97557fa2d9ee8138
SivaGita <sivagi.mu@gmail.com>
2025-08-07 15:43:09 +0530

    API Call getProductMstInfo: Changed 'types' to 'data types' in comment Line

--- opt-mngmnt/getProductMstInfo.js
+++ opt-mngmnt/getProductMstInfo.js
@@ -9,7 +9,7 @@ sql.connect(dbConfig).then(() => {
             
             const request = new sql.Request();
 
-            // Use correct types based on SQL Server stored procedure
+            // Use correct data types based on SQL Server stored procedure
             request.input('Game_Id', sql.NVarChar, req.query.gameId);
             request.input('Game_Batch', sql.Int, parseInt(req.query.gameBatch));
             request.input('Game_Team', sql.NVarChar, req.query.gameTeam);
