1420b4eee05da745bec3d168b3e51d0f191dd457
Ramana <muthuramana96@gmail.com>
2025-08-18 21:26:51 +0530

    fixed syntax issue in game team play

--- opt-mngmnt/updateGameTeamPlay/updateGameTeamPlay.js
+++ opt-mngmnt/updateGameTeamPlay/updateGameTeamPlay.js
@@ -6,67 +6,27 @@ const router = express.Router();
 sql.connect(dbConfig).then(() => {
     router.post('/updateGameTeamPlay', async (req, res) => {
         if (req?.body?.cmdLine) {
-            if (req?.body?.cmdLine === 'Team Play') {
-                try {
-                    /*
-                    const framedQuery = `
-                        EXEC [dbo].[Team_0_Play]
-                            @Game_Id = ${req?.body?.gameId ? `'${req.body.gameId}'` : 'NULL'},
-                            @Game_Batch = ${req?.body?.gameBatch ? `'${req.body.gameBatch}'` : 'NULL'},
-                            @Game_Team = ${req?.body?.gameTeam ? `'${req.body.gameTeam}'` : 'NULL'}
-                        `;
-                    */
-                      const framedQuery = `    
+            try {
+                const framedQuery = `    
                         EXEC [dbo].[UI_Game_Team_Play]
                             @Game_Id = ${req?.body?.gameId ? `'${req.body.gameId}'` : 'NULL'},
                             @Game_Batch = ${req?.body?.gameBatch ? `'${req.body.gameBatch}'` : 'NULL'},
-                            @Game_Team = ${req?.body?.gameTeam ? `'${req.body.gameTeam}'` : 'NULL'}
+                            @Game_Team = ${req?.body?.gameTeam ? `'${req.body.gameTeam}'` : 'NULL'},
                             @CMD_Line = ${req?.body?.cmdline ? `'${req.body.cmdline}'` : 'NULL'}
                         `;
 
-                    const results = await sql.query(framedQuery);
-                    res.json(results.recordset);
-                } catch (err) {
-                    console.error('Query failed:', err);
+                const results = await sql.query(framedQuery);
+                res.json(results.recordset);
+            } catch (err) {
+                console.error('Query failed:', err);
 
-                    if (err.originalError && err.originalError.info && err.originalError.info.message) {
-                        const errorMessage = err.originalError.info.message;
-                        res.status(400).send({ error: errorMessage });
-                    } else {
-                        res.status(500).send('Internal Server Error');
-                    }
-                }
-            } else if (req?.body?.cmdLine === 'Rollback Last Period') {
-                try {
-                    /*
-                    const framedQuery = `
-                        EXEC [dbo].[Team_Rollback_A_Period]
-                            @Game_Id = ${req?.body?.gameId ? `'${req.body.gameId}'` : 'NULL'},
-                            @Game_Batch = ${req?.body?.gameBatch ? `'${req.body.gameBatch}'` : 'NULL'},
-                            @Game_Team = ${req?.body?.gameTeam ? `'${req.body.gameTeam}'` : 'NULL'}
-                        `;
-                    */
-                      const framedQuery = `    
-                        EXEC [dbo].[UI_Game_Team_Play]
-                            @Game_Id = ${req?.body?.gameId ? `'${req.body.gameId}'` : 'NULL'},
-                            @Game_Batch = ${req?.body?.gameBatch ? `'${req.body.gameBatch}'` : 'NULL'},
-                            @Game_Team = ${req?.body?.gameTeam ? `'${req.body.gameTeam}'` : 'NULL'}
-                            @CMD_Line = ${req?.body?.cmdline ? `'${req.body.cmdline}'` : 'NULL'}
-                        `;                    
-                    const results = await sql.query(framedQuery);
-                    res.json(results.recordset);
-                } catch (err) {
-                    console.error('Query failed:', err);
-
-                    if (err.originalError && err.originalError.info && err.originalError.info.message) {
-                        const errorMessage = err.originalError.info.message;
-                        res.status(400).send({ error: errorMessage });
-                    } else {
-                        res.status(500).send('Internal Server Error');
-                    }
+                if (err.originalError && err.originalError.info && err.originalError.info.message) {
+                    const errorMessage = err.originalError.info.message;
+                    res.status(400).send({ error: errorMessage });
+                } else {
+                    res.status(500).send('Internal Server Error');
                 }
             }
-
         } else {
             res.status(400).send('cmdLine is required');
         }
