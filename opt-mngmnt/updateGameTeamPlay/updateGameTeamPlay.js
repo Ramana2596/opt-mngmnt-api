174f598bd94dddabf59a89111d8aa53fc69c9a05
SivaGita <sivagi.mu@gmail.com>
2025-08-30 22:16:36 +0530

    For output parameter: No Value is passed. Just  defined its type:

--- opt-mngmnt/updateGameTeamPlay/updateGameTeamPlay.js
+++ opt-mngmnt/updateGameTeamPlay/updateGameTeamPlay.js
@@ -13,7 +13,7 @@ sql.connect(dbConfig).then(() => {
                 request.input('Game_Id', sql.NVarChar, req?.body?.gameId);
                 request.input('Game_Batch', sql.Int, parseInt(req?.body?.gameBatch));
                 request.input('Game_Team', sql.NVarChar, req?.body?.gameTeam);
-                request.output('Out_Message', sql.NVarChar, req?.body?.outMessage);  // output parameter for message
+                request.output('Out_Message', sql.NVarChar);  // No value, Just define type for output parameter.
                 request.input('CMD_Line', sql.NVarChar, req?.body?.cmdLine);
             
                 const result = await request.execute('UI_Game_Team_Play');
