0116276627036760a677c1b482f836fe232f2151
SivaGita <sivagi.mu@gmail.com>
2025-09-07 15:43:12 +0530

    API Call getFgStockInfo
    Updated for correct data types format for parameters to eliminate error in data conversion

--- opt-mngmnt/getFgStockInfo/getFgStockInfo.js
+++ opt-mngmnt/getFgStockInfo/getFgStockInfo.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const sql = require('mssql');
 const dbConfig = require('../../dbConfig');
@@ -20,3 +21,32 @@ sql.connect(dbConfig).then(() => {
 });
 
 module.exports = router;
+*/
+
+// Updated for correct data types format for parameters
+// to eliminate error in data conversion
+const express = require('express');
+const sql = require('mssql');
+const dbConfig = require('../../dbConfig');
+const router = express.Router();
+
+sql.connect(dbConfig).then(() => {
+  router.get('/getFgStockInfo', async (req, res) => {
+    try {
+        const request = new sql.Request();
+
+        // Add parameters
+        request.input('Game_Id', sql.NVarChar(20), req.query.gameId || null);
+        request.input('Game_Batch', sql.Int, parseInt(req.query.gameBatch) || null);
+        request.input('Game_Team', sql.NVarChar(20), req.query.gameTeam || null);
+
+        const result = await request.execute('dbo.UI_FG_Stock_Info');
+        res.json(result.recordset);
+    } catch (err) {
+      console.error('Query failed:', err);
+      res.status(500).send('Internal Server Error');
+    }   });
+});
+
+module.exports = router;
+
