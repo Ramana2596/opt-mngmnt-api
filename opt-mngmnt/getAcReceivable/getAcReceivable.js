a39c3cae9527dedba9a5aaff6df690129b8c2c76
SivaGita <sivagi.mu@gmail.com>
2025-09-07 18:08:34 +0530

    API Call getAcReceivable
    // Updated for correct data types format for parameters, to eliminate error in data conversion

--- opt-mngmnt/getAcReceivable/getAcReceivable.js
+++ opt-mngmnt/getAcReceivable/getAcReceivable.js
@@ -1,23 +1,29 @@
+// Updated for correct data types format for parameters,
+// to eliminate error in data conversion
+
 const express = require('express');
 const sql = require('mssql');
 const dbConfig = require('../../dbConfig');
 const router = express.Router();
 
+
 sql.connect(dbConfig).then(() => {
-    router.get('/getAcReceivable', async (req, res) => {
-        try {
-            const query = `EXEC [dbo].[UI_Ac_Receivable] 
-                                @Game_Id = '${req.query.gameId}',
-                                @Game_Batch = ${req.query.gameBatch},
-                                @Game_Team = '${req.query.gameTeam}'`;
-            console.log(query);
-            const result = await sql.query(query);
-            res.json(result.recordset);
-        } catch (err) {
-            console.error('Query failed:', err);
-            res.status(500).send('Internal Server Error');
-        }
-    });
+  router.get('/getAcReceivable', async (req, res) => {
+    try {
+        const request = new sql.Request();
+
+        // Add parameters
+        request.input('Game_Id', sql.NVarChar, req.query.gameId || null);
+        request.input('Game_Batch', sql.Int, parseInt(req.query.gameBatch) || null);
+        request.input('Game_Team', sql.NVarChar, req.query.gameTeam || null);
+
+        const result = await request.execute('UI_Ac_Receivable');
+
+        res.json(result.recordset);
+    } catch (err) {
+      console.error('Query failed:', err);
+      res.status(500).send('Internal Server Error');
+    }   });
 });
 
 module.exports = router;
