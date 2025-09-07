d16a360a174d3f561a8790551b2c2086daba0ac8
SivaGita <sivagi.mu@gmail.com>
2025-09-07 16:55:40 +0530

    API Call getAssetCatalogBatch
    Updated for correct data types format for parameters to eliminate error in data conversion

--- opt-mngmnt/getAssetCatalogBatch.js
+++ opt-mngmnt/getAssetCatalogBatch.js
@@ -1,8 +1,10 @@
+// Updated for correct data types format for parameters
+// to eliminate error in data conversion
 const express = require('express');
 const sql = require('mssql');
 const dbConfig = require('../dbConfig');
 const router = express.Router();
-
+/*
 sql.connect(dbConfig).then(() => {
     router.get('/getAssetCatalogBatch', async (req, res) => {
         try {
@@ -11,6 +13,30 @@ sql.connect(dbConfig).then(() => {
                         @Game_Id = ${req?.query?.gameId ? `${req.query.gameId}` : 'NULL'},
                         @Game_Batch = ${req?.query?.gameBatch ? req.query.gameBatch : 'NULL'},
                         @CMD_Line = '${req.query.cmdLine}'`);
+            res.json(result.recordset);
+        } catch (err) {
+            console.error('Query failed:', err);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+});
+
+module.exports = router;
+*/
+
+sql.connect(dbConfig).then(() => {
+    router.get('/getAssetCatalogBatch', async (req, res) => {
+        try {
+    
+            const request = new sql.Request();
+
+            // Use correct types based on SQL Server stored procedure
+            request.input('Game_Id', sql.NVarChar(20), req.query.gameId || null);
+            request.input('Game_Batch', sql.Int, parseInt(req.query.gameBatch) || null);
+            request.input('CMD_Line', sql.NVarChar, req.query.cmdLine)|| null);
+
+            const result = await request.execute('UI_Asset_Catalog_Batch');
+
             res.json(result.recordset);
         } catch (err) {
             console.error('Query failed:', err);
