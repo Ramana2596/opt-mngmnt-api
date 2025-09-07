7b5c7f4dcf76c2bfaa5ffa32216023a05d965399
SivaGita <sivagi.mu@gmail.com>
2025-09-07 17:09:27 +0530

    API Call getAssetCatalog Syntax error inn parameter declaration corrected

--- opt-mngmnt/getAssetCatalog.js
+++ opt-mngmnt/getAssetCatalog.js
@@ -10,9 +10,10 @@ sql.connect(dbConfig).then(() => {
             const request = new sql.Request();
 
             // Use correct types based on SQL Server stored procedure
-            request.input('Game_Id', sql.NVarChar, req.query.gameId)|| null);
-            request.input('Game_Batch', sql.Int, parseInt(req.query.gameBatch))|| null);
-            request.input('Game_Team', sql.NVarChar, req.query.gameTeam)|| null);
+
+            request.input('Game_Id', sql.NVarChar, req.query.gameId|| null);
+            request.input('Game_Batch', sql.Int, parseInt(req.query.gameBatch)|| null);
+            request.input('Game_Team', sql.NVarChar, req.query.gameTeam|| null);
 
             const result = await request.execute('UI_Asset_Catalog');
 
