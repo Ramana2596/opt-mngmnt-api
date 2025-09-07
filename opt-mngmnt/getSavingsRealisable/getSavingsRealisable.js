99fca6cc311766b5e23c2621cc423e6c4104ea4c
SivaGita <sivagi.mu@gmail.com>
2025-09-07 19:41:26 +0530

    API Call getSavingsRealisable
    Updated for correct data types format for parameters, to eliminate error in data conversion

--- opt-mngmnt/getSavingsRealisable/getSavingsRealisable.js
+++ opt-mngmnt/getSavingsRealisable/getSavingsRealisable.js
@@ -1,3 +1,6 @@
+// Updated for correct data types format for parameters
+// to eliminate error in data conversion
+
 const express = require('express');
 const sql = require('mssql');
 const dbConfig = require('../../dbConfig');
@@ -6,17 +9,21 @@ const router = express.Router();
 sql.connect(dbConfig).then(() => {
     router.get('/getSavingsRealisable', async (req, res) => {
         try {
-            const result = await sql.query(`
-   EXEC [dbo].[UI_Savings_Realisable]
-        @Game_Id = '${req.query.gameId}',
-        @Game_Batch = ${req.query.gameBatch},
-        @Game_Team = '${req.query.gameTeam}'`);
+
+            const request = new sql.Request();
+
+            // Use correct data types based on SQL Server stored procedure
+            request.input('Game_Id', sql.NVarChar, req.query.gameId || null);
+            request.input('Game_Batch', sql.Int, parseInt(req.query.gameBatch || null));
+            request.input('Game_Team', sql.NVarChar, req.query.gameTeam || null); 
+
+            const result = await request.execute('UI_Savings_Realisable');
+
             res.json(result.recordset);
-        } catch (err) {
+        }   catch (err) {
             console.error('Query failed:', err);
             res.status(500).send('Internal Server Error');
-        }
-    });
+        }   });
 });
 
 module.exports = router;
