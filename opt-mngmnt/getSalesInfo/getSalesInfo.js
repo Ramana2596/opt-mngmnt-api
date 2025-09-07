34d6045a682c9e44ef729e1a067a3331ad643b81
SivaGita <sivagi.mu@gmail.com>
2025-09-07 17:44:31 +0530

    API Call gettSalesInfo
    Updated for correct data types format for parameters to eliminate error in data conversion

--- opt-mngmnt/getSalesInfo/getSalesInfo.js
+++ opt-mngmnt/getSalesInfo/getSalesInfo.js
@@ -1,3 +1,6 @@
+// Updated for correct data types format for parameters
+// to eliminate error in data conversion
+
 const express = require('express');
 const sql = require('mssql');
 const dbConfig = require('../../dbConfig');
@@ -6,17 +9,19 @@ const router = express.Router();
 sql.connect(dbConfig).then(() => {
     router.get('/getSalesInfo', async (req, res) => {
         try {
-            const result = await sql.query(`
-   EXEC [dbo].[UI_Sales_Record_Info] 
-        @Game_Id = '${req.query.gameId}',
-        @Game_Batch = ${req.query.gameBatch},
-        @Game_Team = '${req.query.gameTeam}'`);
+            const request = new sql.Request();
+            // Add parameters
+            request.input('Game_Id', sql.NVarChar, req.query.gameId || null);
+            request.input('Game_Batch', sql.Int, parseInt(req.query.gameBatch) || null);
+            request.input('Game_Team', sql.NVarChar, req.query.gameTeam || null);
+
+            const result = await request.execute('UI_Sales_Record_Info');
+
             res.json(result.recordset);
         } catch (err) {
             console.error('Query failed:', err);
             res.status(500).send('Internal Server Error');
-        }
-    });
+        }   });
 });
 
 module.exports = router;
