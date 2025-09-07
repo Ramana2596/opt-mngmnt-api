3c9b0ea7270c4b9b6ecc4281a21a3f51aad0905d
SivaGita <sivagi.mu@gmail.com>
2025-09-07 16:29:02 +0530

    The updated codelines are okay. Test: Respective UI Screen  opens in browser So, the existing codelines that were commented lines are deleted

--- opt-mngmnt/getClassRoomSession.js
+++ opt-mngmnt/getClassRoomSession.js
@@ -1,30 +1,4 @@
-/*
 
-const express = require('express');
-const sql = require('mssql');
-const dbConfig = require('../dbConfig');
-const router = express.Router();
-
-sql.connect(dbConfig).then(() => {
-    router.get('/getClassRoomSession', async (req, res) => {
-        try {
-            const result = await sql.query(`
-                EXEC [dbo].[UI_Class_Room_Session]
-                    @Game_Id = ${req?.query?.gameId ? `${req.query.gameId}` : 'NULL'},
-                    @Game_Batch = ${req?.query?.gameBatch ? req.query.gameBatch : 'NULL'}`);
-                //    @Game_Id = '${req.query.gameId}',
-                //    @Game_Batch = ${req.query.gameBatch}`);
-            console.log(result);
-            res.json(result.recordset);
-        } catch (err) {
-            console.error('Query failed:', err);
-            res.status(500).send('Internal Server Error');
-        }
-    });
-});
-
-module.exports = router;
-*/
 // Updated for correct data types format
 const express = require('express');
 const sql = require('mssql');
--- opt-mngmnt/getRmStockInfo/getRmStockInfo.js
+++ opt-mngmnt/getRmStockInfo/getRmStockInfo.js
@@ -1,28 +1,3 @@
-/*
-
-const express = require('express');
-const sql = require('mssql');
-const dbConfig = require('../../dbConfig');
-const router = express.Router();
-
-sql.connect(dbConfig).then(() => {
-    router.get('/getFgStockInfo', async (req, res) => {
-        try {
-            const result = await sql.query(`
-   EXEC [dbo].[UI_FG_Stock_Info] 
-        @Game_Id = '${req.query.gameId}',
-        @Game_Batch = ${req.query.gameBatch},
-        @Game_Team = '${req.query.gameTeam}'`);
-            res.json(result.recordset);
-        } catch (err) {
-            console.error('Query failed:', err);
-            res.status(500).send('Internal Server Error');
-        }
-    });
-});
-
-module.exports = router;
-*/
 
 // Updated for correct data types format for parameters
 // to eliminate error in data conversion
