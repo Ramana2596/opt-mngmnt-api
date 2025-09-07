688e8b6e39758039b6e0fa620b154fecbf40888c
SivaGita <sivagi.mu@gmail.com>
2025-09-07 13:39:19 +0530

    API Call getClassRoomSession: Updated for correct data type format to eleimnate error in data conversion

--- opt-mngmnt/getClassRoomSession.js
+++ opt-mngmnt/getClassRoomSession.js
@@ -1,3 +1,5 @@
+/*
+
 const express = require('express');
 const sql = require('mssql');
 const dbConfig = require('../dbConfig');
@@ -19,6 +21,32 @@ sql.connect(dbConfig).then(() => {
             res.status(500).send('Internal Server Error');
         }
     });
+});
+
+module.exports = router;
+*/
+// Updated for correct data types format
+const express = require('express');
+const sql = require('mssql');
+const dbConfig = require('../dbConfig');
+const router = express.Router();
+
+sql.connect(dbConfig).then(() => {
+  router.get('/getClassRoomSession', async (req, res) => {
+    try {
+      const request = new sql.Request();
+
+      // Add parameters
+      request.input('Game_Id', sql.NVarChar(20), req.query.gameId || null);
+      request.input('Game_Batch', sql.Int, parseInt(req.query.gameBatch) || null);
+
+      const result = await request.execute('dbo.UI_Class_Room_Session');
+      res.json(result.recordset);
+    } catch (err) {
+      console.error('Query failed:', err);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 });
 
 module.exports = router;
