44923355e62182b55de693972321e20705ae913b
SivaGita <sivagi.mu@gmail.com>
2025-09-07 13:12:02 +0530

    Aligned lines

--- opt-mngmnt/getMarketInfoTeam.js
+++ opt-mngmnt/getMarketInfoTeam.js
@@ -8,12 +8,12 @@ sql.connect(dbConfig).then(() => {
     router.get('/getMarketInfoTeam', async (req, res) => {
         try {
             const result = await sql.query(`
-   EXEC [dbo].[UI_Market_Info_Team]
-        @Game_Id = '${req.query.gameId}',
-        @Game_Batch = ${req.query.gameBatch},
-        @Game_Team = '${req.query.gameTeam}'`);
+                EXEC [dbo].[UI_Market_Info_Team]
+                    @Game_Id = '${req.query.gameId}',
+                    @Game_Batch = ${req.query.gameBatch},
+                    @Game_Team = '${req.query.gameTeam}'`);
             res.json(result.recordset);
-        } catch (err) {
+            } catch (err) {
             console.error('Query failed:', err);
             res.status(500).send('Internal Server Error');
         }
