04b703e160ec27bc2e5f61554669acc1db1c2089
SivaGita <sivagi.mu@gmail.com>
2025-09-07 19:08:17 +0530

    API Call getOpsDemandCreationInfo
    Updated for correct data types format for parameters, to eliminate error in data conversion

--- opt-mngmnt/getOpsDemandCreationInfo.js
+++ opt-mngmnt/getOpsDemandCreationInfo.js
@@ -1,22 +1,30 @@
+// Updated for correct data types format for parameters
+// to eliminate error in data conversion
+
 const express = require('express');
 const sql = require('mssql');
 const dbConfig = require('../dbConfig');
 const router = express.Router();
 
+
 sql.connect(dbConfig).then(() => {
     router.get('/getOpsDemandCreationInfo', async (req, res) => {
         try {
-            const result = await sql.query(`
-   EXEC [dbo].[UI_Ops_Demand_Creation_Info]
-        @Game_Id = '${req.query.gameId}',
-        @Game_Batch = ${req.query.gameBatch},
-        @Game_Team = '${req.query.gameTeam}'`);
+
+            const request = new sql.Request();
+
+            // Use correct data types based on SQL Server stored procedure
+            request.input('Game_Id', sql.NVarChar, req.query.gameId || null);
+            request.input('Game_Batch', sql.Int, parseInt(req.query.gameBatch || null));
+            request.input('Game_Team', sql.NVarChar, req.query.gameTeam || null);
+
+            const result = await request.execute('UI_Ops_Demand_Creation_Info');
+
             res.json(result.recordset);
         } catch (err) {
             console.error('Query failed:', err);
             res.status(500).send('Internal Server Error');
-        }
-    });
+        }   });
 });
 
-module.exports = router;
+module.exports = router;    
