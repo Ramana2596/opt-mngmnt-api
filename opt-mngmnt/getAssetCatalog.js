50cf91f201b00427d0ae5ce636936e85d1bca00c
SivaGita <sivagi.mu@gmail.com>
2025-09-07 16:53:15 +0530

    API Call getAssetCatalog
    Updated for correct data types format for parameters to eliminate error in data conversion

--- opt-mngmnt/getAssetCatalog.js
+++ opt-mngmnt/getAssetCatalog.js
@@ -10,9 +10,9 @@ sql.connect(dbConfig).then(() => {
             const request = new sql.Request();
 
             // Use correct types based on SQL Server stored procedure
-            request.input('Game_Id', sql.NVarChar, req.query.gameId);
-            request.input('Game_Batch', sql.Int, parseInt(req.query.gameBatch));
-            request.input('Game_Team', sql.NVarChar, req.query.gameTeam);
+            request.input('Game_Id', sql.NVarChar, req.query.gameId)|| null);
+            request.input('Game_Batch', sql.Int, parseInt(req.query.gameBatch))|| null);
+            request.input('Game_Team', sql.NVarChar, req.query.gameTeam)|| null);
 
             const result = await request.execute('UI_Asset_Catalog');
 
