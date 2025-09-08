2de66a60ae90424f00dd865976e9baf26e169798
SivaGita <sivagi.mu@gmail.com>
2025-09-08 16:26:51 +0530

    API Call getOpereationnInputError
    Udpated : Parameters, validating Data types as in SP

--- opt-mngmnt/getOperationInputError.js
+++ opt-mngmnt/getOperationInputError.js
@@ -1,16 +1,24 @@
+// Updated for correct data types format for parameters
+// to eliminate error in data conversion
+
 const express = require('express');
 const sql = require('mssql');
 const dbConfig = require('../dbConfig');
 const router = express.Router();
 
+
 sql.connect(dbConfig).then(() => {
     router.get('/getOperationInputError', async (req, res) => {
         try {
-            const result = await sql.query(`
-   EXEC [dbo].[UI_Operation_Input_Error]
-        @Game_Id = '${req.query.gameId}',
-        @Game_Batch = ${req.query.gameBatch},
-        @Game_Team = '${req.query.gameTeam}'`);
+            const request = new sql.Request();  
+
+            // Add Parameters, validating Data types as in SP
+            request.input('Game_Id', sql.NVarChar, req.query.gameId  || null);
+            request.input('Game_Batch', sql.Int, parseInt(req.query.gameBatch)  || null);
+            request.input('Game_Team', sql.NVarChar, req.query.gameTeam || null);
+
+            const result = await request.execute('UI_Operation_Input_Error');
+
             res.json(result.recordset);
         } catch (err) {
             console.error('Query failed:', err);
@@ -20,3 +28,5 @@ sql.connect(dbConfig).then(() => {
 });
 
 module.exports = router;
+
+
