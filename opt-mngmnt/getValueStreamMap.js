af7adbe28748ef6316555ecb680227711c227bb4
SivaGita <sivagi.mu@gmail.com>
2025-09-08 21:54:05 +0530

    API Call VAlueStreamMap
    Updated: Parameterised query, validating data types

--- opt-mngmnt/getValueStreamMap.js
+++ opt-mngmnt/getValueStreamMap.js
@@ -1,15 +1,21 @@
+// Updated: Parameterised query, validating data types
+
 const express = require('express');
 const sql = require('mssql');
 const dbConfig = require('../dbConfig');
 const router = express.Router();
 
+
 sql.connect(dbConfig).then(() => {
     router.get('/getValueStreamMap', async (req, res) => {
         try {
-            const result = await sql.query(`
-   EXEC [dbo].[UI_Value_Stream_Map]
-        @Game_Id = '${req.query.gameId}'
-       `);
+            const request = new sql.Request();
+
+            // Add Parameters, validating Data types as in SP
+            request.input('Game_Id', sql.NVarChar, req.query.gameId || null);
+
+            const result = await request.execute('UI_Value_Stream_Map');
+
             res.json(result.recordset);
         } catch (err) {
             console.error('Query failed:', err);
