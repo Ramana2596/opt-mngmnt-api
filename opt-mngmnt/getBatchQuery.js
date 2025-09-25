29e5588bf33071625f7cfb98f4df856955f9b0ed
SivaGita <sivagi.mu@gmail.com>
2025-09-25 13:15:34 +0530

    API Call getBatchQuery
    Added for UI Batch Mgt

---
+++ opt-mngmnt/getBatchQuery.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const sql = require('mssql');
+const dbConfig = require('../dbConfig');
+const router = express.Router();
+
+
+sql.connect(dbConfig).then(() => {
+    router.get('/getBatchQuery', async (req, res) => {
+        try {
+            
+            const request = new sql.Request();
+
+            // Use correct types based on SQL Server stored procedure
+            request.input('Game_Id', sql.NVarChar, req.query.gameId  || null);
+            request.input('Game_Batch', sql.Int, parseInt(req.query.gameBatch)  || null);
+            request.input('Game_Team', sql.NVarChar, req.query.gameTeam || null);
+            request.input('CMD_Line', sql.NVarChar, req.query.cmdLine|| null);
+
+            const result = await request.execute('UI_Batch_Query');
+
+            res.json(result.recordset);
+        } catch (err) {
+            console.error('Query failed:', err);
+            res.status(500).send('Internal Server Error');
+        }
+    });
+});
+
+module.exports = router;
