5478998a6766d9c1fa3056c0707f028aa0e636da
SivaGita <sivagi.mu@gmail.com>
2025-10-12 23:11:27 +0530

    API Call updateBatchMst
    Console log reoved.
    
    The parameter name case-style is by FORM -SP signature. So camelcase codes are replaced with Snake_Case style.
    
    Test: OKAY
    The Values get populated and tables get updated.

--- opt-mngmnt/updateBatchMst.js
+++ opt-mngmnt/updateBatchMst.js
@@ -10,28 +10,13 @@ const router = express.Router();
 router.post('/updateBatchMst', async (req, res) => {
 
   try {
-    console.log('bathData received:', req.body);
     // Connect to SQL Server
     const pool = await sql.connect(dbConfig);
     const request = pool.request();
 
     const batchData = req.body;
-/*
-    // Input parameters (match SP signature)
-    request.input("Game_Id", sql.NVarChar(20), batchData.gameId);
-    request.input("Game_Batch", sql.Int, batchData.gameBatch);
-    request.input("Centre_Id", sql.Int, batchData.centreId);
-    request.input("Faculty", sql.NVarChar(50), batchData.faculty);
-    request.input("Facilitator", sql.NVarChar(50), batchData.facilitator);
-    request.input("Venue", sql.NVarChar(100), batchData.venue);
-    request.input("Start_Date", sql.Date, batchData.startDate);
-    request.input("Duration", sql.SmallInt, batchData.duration);
-    request.input("UOM", sql.NVarChar(10), batchData.uom);
-    request.input("Close_Date", sql.Date, batchData.closeDate);
-    request.input("Batch_Status", sql.NVarChar(20), batchData.batchStatus);
-    //request.input("CMD_Line", sql.NVarChar(20), batchData.CMD_Line || "Update");
-*/
-//  Input parameters (match SP signature and payload keys)
+
+//  Input parameters (match SP signature and Form/payload keys)
     request.input("Game_Id", sql.NVarChar(20), batchData.Game_Id);
     request.input("Game_Batch", sql.Int, batchData.Game_Batch);
     request.input("Centre_Id", sql.Int, batchData.Centre_Id);
@@ -46,23 +31,6 @@ router.post('/updateBatchMst', async (req, res) => {
     // Output parameter
     request.output("Out_Message", sql.NVarChar(200));
 
-    // --- Log SQL Parameters ---
-        const sqlLog = (`
-    EXEC UI_Batch_Mst_Trans
-        @Game_Id = N'${batchData.gameId}',
-        @Game_Batch = ${batchData.gameBatch},
-        @Centre_Id = ${batchData.centreId},
-        @Faculty = N'${batchData.faculty}',
-        @Facilitator = N'${batchData.facilitator}',
-        @Venue = N'${batchData.venue}',
-        @Start_Date = '${batchData.startDate}',
-        @Duration = ${batchData.duration},
-        @UOM = N'${batchData.uom}',
-        @Close_Date = '${batchData.closeDate}',
-        @Batch_Status = N'${batchData.batchStatus}',
-        @Out_Message = OUTPUT;
-    `);
-        console.log('📝 Executing SQL:\n', sqlLog);
     // Execute stored procedure
     const result = await request.execute("UI_Batch_Mst_Trans");
 
