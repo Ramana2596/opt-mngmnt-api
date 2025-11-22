58b4bb312efad09cea6b85f20b71c29eadbe0753
SivaGita <sivagi.mu@gmail.com>
2025-11-22 17:30:07 +0530

    API Call getBalanceSheetInfo
    Added Parameter Datatype check to eliminate SQL Injection
    
    SP TEST:  Okay
    
    Game_Batch / Game_Team :
       NULL -  Out Message  displayed
      NOT NULL: SQL query executes

--- opt-mngmnt/getBalanceSheetInfo.js
+++ opt-mngmnt/getBalanceSheetInfo.js
@@ -1,3 +1,5 @@
+/*
+
 const express = require('express');
 const sql = require('mssql');
 const dbConfig = require('../dbConfig');
@@ -17,6 +19,37 @@ sql.connect(dbConfig).then(() => {
                 res.status(500).send('Internal Server Error');
             }
     });
+});
+
+module.exports = router;
+*/
+
+// Updated for correct data types format for parameters
+// to eliminate error in data conversion
+
+const express = require('express');
+const sql = require('mssql');
+const dbConfig = require('../dbConfig');
+const router = express.Router();
+
+sql.connect(dbConfig).then(() => {
+    router.get('/getBalanceSheetInfo', async (req, res) => {
+        try {
+
+            const request = new sql.Request();
+
+            // Use correct data types based on SQL Server stored procedure
+            request.input('Game_Id', sql.NVarChar, req.query.gameId || null);
+            request.input('Game_Batch', sql.Int, parseInt(req.query.gameBatch || null));
+            request.input('Game_Team', sql.NVarChar, req.query.gameTeam || null);   
+
+            const result = await request.execute('UI_Balance_Sheet_Info');
+
+            res.json(result.recordset);
+        }   catch (err) {
+            console.error('Query failed:', err);
+            res.status(500).send('Internal Server Error');
+        }   });
 });
 
 module.exports = router;
