cebf00ecd8c35b81eb8aacd0a556ac7963a1dbea
SivaGita <sivagi.mu@gmail.com>
2025-11-22 19:01:58 +0530

    getCashFlowStatementInfo
    Updated for correct data types format for parameters

--- opt-mngmnt/getCashFlowStatement/getCashFlowStatement.js
+++ opt-mngmnt/getCashFlowStatement/getCashFlowStatement.js
@@ -1,16 +1,26 @@
+
+
+// Updated for correct data types format for parameters
+// to eliminate error in data conversion
+
 const express = require('express');
 const sql = require('mssql');
-const dbConfig = require('../../dbConfig');
+const dbConfig = require('../dbConfig');
 const router = express.Router();
 
 sql.connect(dbConfig).then(() => {
     router.get('/getCashFlowStatement', async (req, res) => {
         try {
-            const result = await sql.query(`
-   EXEC [dbo].[UI_CF_Statement_Info] 
-        @Game_Id = '${req.query.gameId}',
-        @Game_Batch = ${req.query.gameBatch},
-        @Game_Team = '${req.query.gameTeam}'`);
+
+            const request = new sql.Request();
+
+            // Use correct data types based on SQL Server stored procedure
+            request.input('Game_Id', sql.NVarChar, req.query.gameId || null);
+            request.input('Game_Batch', sql.Int, parseInt(req.query.gameBatch || null));
+            request.input('Game_Team', sql.NVarChar, req.query.gameTeam || null);
+
+            const result = await request.execute('UI_CF_Statement_Info');
+
             res.json(result.recordset);
         } catch (err) {
             console.error('Query failed:', err);
