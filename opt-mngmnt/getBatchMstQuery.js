20f25dda881ac2ff6a937e3c255c655ee192947d
SivaGita <sivagi.mu@gmail.com>
2026-04-19 08:07:10 +0530

    API getBatchMstQuery
    Changes:
    --dbConfig is at app level and Not reqd at each API.
    -- sql.connect is commented
    --Correct  router.post to getBatchMstQuery

--- opt-mngmnt/getBatchMstQuery.js
+++ opt-mngmnt/getBatchMstQuery.js
@@ -1,12 +1,12 @@
 // SQL Query Construct Format request.execute ( ..... )
 const express = require('express');
 const sql = require('mssql');
-const dbConfig = require('../dbConfig');
+//const dbConfig = require('../dbConfig');
 const router = express.Router();
 
 
-sql.connect(dbConfig).then(() => {
-    router.post('/getBatchQuery', async (req, res) => {
+//sql.connect(dbConfig).then(() => {
+    router.post('/getBatchMstQuery', async (req, res) => {
         try {
             
             const request = new sql.Request();
@@ -24,47 +24,7 @@ sql.connect(dbConfig).then(() => {
             res.status(500).send('Internal Server Error');
         }
     });
-});
+//});
 
 
 module.exports = router;
-
-// Manual SQL Query Construct format
-/*
-const express = require('express');
-const sql = require('mssql');
-const dbConfig = require('../dbConfig');
-const router = express.Router();
-
-sql.connect(dbConfig).then(() => {
-    router.post('/getBatchQuery', async (req, res) => {
-        try {
-            const gameId = req.body.gameId || null;
-            const gameBatch = req.body.gameBatch ? parseInt(req.body.gameBatch) : null;
-            const cmdLine = req.body.cmdLine || null;
-
-            // Construct SQL string safely (parameterized)
-            const query = `
-                EXEC UI_Batch_Query 
-                    @Game_Id = ${gameId ? `'${gameId}'` : 'NULL'}, 
-                    @Game_Batch = ${gameBatch !== null ? gameBatch : 'NULL'}, 
-                    @CMD_Line = ${cmdLine ? `'${cmdLine}'` : 'NULL'}
-            `;
-
-            // Log query string
-            console.log("Running query:\n", query);
-
-            // Execute query
-            const result = await sql.query(query);
-
-            // Log response
-            console.log("Query result:", result.recordset);
-
-            res.json(result.recordset);
-        } catch (err) {
-            console.error("Query failed:", err);
-            res.status(500).send("Internal Server Error");
-        }
-    });
-});
-*/
