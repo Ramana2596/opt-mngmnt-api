396b0ce4bb963d0b343cdcac19bdfefff731b7d0
SivaGita <sivagi.mu@gmail.com>
2026-09-10 20:24:11 +0530

    API getStraategyPlan
    Changes:
    -- API template used.
    -- API/GET
    -- Datatype validated

--- opt-mngmnt/getStrategyPlan.js
+++ opt-mngmnt/getStrategyPlan.js
@@ -1,60 +1,29 @@
 const express = require('express');
 const sql = require('mssql');
-const dbConfig = require('../dbConfig');
 const router = express.Router();
 
-/*
-sql.connect(dbConfig).then(() => {
-    router.get('/getStrategyPlan', async (req, res) => {
-        if (req.query.type === 'getStrategyPlan') {
-            try {
-                const result = await sql.query(`DECLARE @Game_Id NVARCHAR(20)
-    DECLARE @Game_Batch SMALLINT
-    DECLARE @Game_Team NVARCHAR(20)
-    DECLARE @CMD_Line NVARCHAR(100)
-    SET @Game_Id = '${req.query.gameId}'
-    SET @Game_Batch = ${req.query.gameBatch}
-    SET @Game_Team = '${req.query.gameTeam}'
-    SET @CMD_Line = 'Get_Strategy_Plan'
-    EXEC [dbo].[UI_Strategy_Plan_Query] 
-        @Game_Id = @Game_Id,
-        @Game_Batch = @Game_Batch,
-        @Game_Team = @Game_Team,
-        @CMD_Line = @CMD_Line`);
-                res.json(result.recordset);
-            } catch (err) {
-                console.error('Query failed:', err);
-                res.status(500).send('Internal Server Error');
-            }
-        }
+router.get('/getStrategyPlan', async (req, res) => {
+  try {
+    const request = new sql.Request();
+
+    request.input('Game_Id', sql.NVarChar, req.query.gameId || null);
+    request.input('Game_Batch', sql.Int, parseInt(req.query.gameBatch) || null);
+    request.input('Game_Team', sql.NVarChar, req.query.gameTeam || null);
+    request.input('CMD_Line', sql.NVarChar, req.query.cmdLine || null);
+
+    request.output('Out_Message', sql.NVarChar(200));
+
+    const result = await request.execute('UI_Strategy_Plan_Query');
+
+    res.json({
+      data: result.recordset || [],
+      Out_Message: result.output?.Out_Message || null,
+      returnValue: result.returnValue,
     });
-});
-
-module.exports = router;
-*/
-sql.connect(dbConfig).then(() => {
-    router.get('/getStrategyPlan', async (req, res) => {
-        try {
-            
-            const request = new sql.Request();
-
-            // Use correct types based on SQL Server stored procedure
-            request.input('Game_Id', sql.NVarChar, req.query.gameId  || null);
-            request.input('Game_Batch', sql.Int, parseInt(req.query.gameBatch)  || null);
-            request.input('Game_Team', sql.NVarChar, req.query.gameTeam || null);
-            request.input('CMD_Line', sql.NVarChar, req.query.cmdLine|| null);
-
-            // SP output parameter
-            request.output('Out_Message', sql.NVarChar(200));
-            
-            const result = await request.execute('UI_Strategy_Plan_Query');
-
-            res.json(result.recordset);
-        } catch (err) {
-            console.error('Query failed:', err);
-            res.status(500).send('Internal Server Error');
-        }
-    });
+  } catch (err) {
+    console.error('Query failed:', err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 module.exports = router;
